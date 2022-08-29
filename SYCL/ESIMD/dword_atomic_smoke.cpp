e3e8f3948656455e1524730d0c7b87ee973c54f1
Ahn, Dongkyun <dongkyun.ahn@intel.com>
2022-08-29 12:33:41 -0700

    [SYCL][ESIMD][EMU] Disabling ESIMD_EMULATOR for dword_atomic test
    - Unsupported __esimd_svm_atomic0/1/2 intrinsic implemenations cause
    runtime failure from 'dword_atomic_smoke.cpp'
    
    - XFAIL marking for the test

--- SYCL/ESIMD/dword_atomic_smoke.cpp
+++ SYCL/ESIMD/dword_atomic_smoke.cpp
@@ -8,6 +8,8 @@
 // This test checks DWORD atomic operations.
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
+// TODO: esimd_emulator fails due to unsupported __esimd_svm_atomic0/1/2
+// XFAIL: esimd_emulator
 // UNSUPPORTED: cuda || hip
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
